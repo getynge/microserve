43303b9e9822652ef8820701bd4e4646df10133c
getynge <sparklytarnish+git@protonmail.com>
2018-11-07 19:55:37 -0500

    port and file directory can now be configured

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+extern crate clap;
 extern crate hyper;
 extern crate futures;
 
@@ -6,13 +8,14 @@ use std::alloc::System;
 use std::fs::File;
 use std::io::Read;
 use hyper::{service::service_fn, Method, Body, Request, Response, Server, StatusCode};
+use clap::App;
 
 #[global_allocator]
 static GLOBAL: System = System;
 
 type BoxFut = Box<Future<Item = Response<Body>, Error = hyper::Error> + Send>;
 
-fn get_file(req: Request<Body>) -> BoxFut {
+fn get_file(base_path: String, req: Request<Body>) -> BoxFut {
     let mut response = Response::new(Body::empty());
 
     match req.method() {
@@ -20,9 +23,9 @@ fn get_file(req: Request<Body>) -> BoxFut {
             let uri = req.uri().path();
             let path;
             if uri.ends_with("/") {
-                path = format!("./www/{}/index.html", uri)
+                path = format!("{}/{}/index.html", base_path, uri)
             } else {
-                path = format!("./www/{}", uri)
+                path = format!("{}/{}", base_path, uri)
             }
             if let Ok(mut file) = File::open(path) {
                 let mut contents = String::new();
@@ -43,11 +46,21 @@ fn get_file(req: Request<Body>) -> BoxFut {
 }
 
 fn main() {
-    let addr = ([0, 0, 0, 0], 80).into();
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    let base_path = matches.value_of("base_path").unwrap_or("./www/").to_owned();
+    let announced_path = base_path.clone();
+    let port = matches.value_of("port").unwrap_or("80").parse::<u16>().unwrap_or(80);
+    let addr = ([0, 0, 0, 0], port).into();
+
     let server = Server::bind(&addr)
-        .serve(|| service_fn(get_file))
+        .serve(move || {
+            let copy = base_path.clone();
+            service_fn(move |r| get_file(copy.clone(), r))
+        })
         .map_err(|e| eprintln!("error: {}", e));
 
-    println!("Serving on port 80");
+    println!("Serving directory {} on port {}", announced_path, port);
     hyper::rt::run(server)
 }
